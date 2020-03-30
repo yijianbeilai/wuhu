5a77d832f17479242c4066b9d774a477e0fa3493
xmy <2534890334@qq.com>
2020-03-30 12:26:11 +0800

    创建新项目wuhu提交

---
+++ src/main/java/com/hz/wuhu/WuhuApplication.java
@@ -0,0 +1,13 @@
+package com.hz.wuhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuhuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WuhuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hz/wuhu/WuhuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hz.wuhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuhuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
